e00322e2ea31e4c929e9b7cbdd7eacd1be638a23
Zerbix <zerbix@outlook.com>
2016-10-24 00:20:54 +0200

    Added some prototype files
    First HTML Prototype with php includes.

---
+++ source/view/passwordforgotten.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+    <title>Stage SQUIRREL - Start</title>
+    <link rel="stylesheet" href="stylesheets/main.css">
+  </head>
+  <body>
+	<?php include 'header.php' ?>
+		<p style="color:white;">This could be the starting page.</p>
+	</div>
+  </body>
+</html>
