3153ce1d742d9d1068498180f10af543b4646ffe
blackfurball <github@fellowe.net>
2017-06-13 20:52:23 +0200

    Added option for interface to listen on

--- server.js
+++ server.js
@@ -5,6 +5,9 @@
 
 var express  = require('express');
 var app      = express();
+// choose on which interface and port stage-squirrel will listen.
+// Set ip to '0.0.0.0' for all interfaces
+var ip       = process.env.IP || '127.0.0.1';
 var port     = process.env.PORT || 8500;
 //  var mongoose = require('mongoose');
 var mysql    = require('mysql');
@@ -56,5 +59,5 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
-app.listen(port);
-console.log('The magic happens on port ' + port);
+app.listen(port, ip);
+console.log('The magic happens at ' + ip + ':' + port);
