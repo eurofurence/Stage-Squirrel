347da6f16b1293335c4d53f3a8feaa128f96d86e
Kandril <kandril.kanusky@gmail.com>
2017-07-14 00:41:06 +0200

    #15 Move /home route to separate file

--- app/routes.js
+++ app/routes.js
@@ -20,35 +20,9 @@ module.exports = function(app, passport) {
         res.render('login.ejs', { message: req.flash('loginMessage') }); // load the index.ejs file
     });
 
+    require('./routes/home')(app, passport, connection);
     require('./routes/login')(app, passport);
     require('./routes/signup')(app, passport, connection);
-
-    // =====================================
-    // home SECTION =====================
-    // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/home', isLoggedIn, function(req, res) {
-    connection.query('SELECT * FROM sq_events LEFT JOIN sq_event_details ON sq_events.event_id = sq_event_details.event_id', function (err, eventrows) {
-      connection.query('SELECT * from sq_user', function (err, userrows) {
-        connection.query('SELECT * FROM sq_stage', function (err, stagerows) {
-          connection.query('SELECT * from sq_role', function (err, rolerows) {
-            connection.query('SELECT * from sq_notifications where convention_id = ? order by created_on desc limit 20', req.user.currentConvention.convention_id, function (err, notificationrows) {
-              res.render('home.ejs', {
-                notifications: notificationrows,
-                events: eventrows,
-                users: userrows,
-                stages: stagerows,
-                roles: rolerows,
-                nav: 'home',
-                user : req.user // get the user out of session and pass to template
-              });
-            });
-          });
-        });
-      });
-    });
-    });
 
     // =====================================
     // PROFILE SECTION =====================
