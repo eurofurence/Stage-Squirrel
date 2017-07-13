02a776dadb7f3e3ca2600fb6f7ce45205fc805db
Kandril <kandril.kanusky@gmail.com>
2017-07-14 00:53:50 +0200

    #15 Fix indentation in lib-files

--- app/lib/notificationService.js
+++ app/lib/notificationService.js
@@ -1,16 +1,16 @@
 var util = require('./utilities');
 
 var CHECK_TELEGRAM_ACTIVE_QUERY = [
-  "SELECT configuration_value ",
-  "FROM sq_configuration ",
-  "WHERE configuration_key = 'TELEGRAM_API_TOKEN'"
+    "SELECT configuration_value ",
+    "FROM sq_configuration ",
+    "WHERE configuration_key = 'TELEGRAM_API_TOKEN'"
 ].join('');
 var INSERT_NOTIFICATION_QUERY = [
-  "INSERT INTO sq_notifications",
-    "(`type_id`, `event_id`, `convention_id`, `version_id`, `sender_id`, `role_id`, `created_on`)",
-  " VALUES ",
-    "(?, ?, (SELECT convention_id from sq_events where sq_events.event_id = ?), ?, ?, ?, ?)",
-  ";"
+    "INSERT INTO sq_notifications",
+        "(`type_id`, `event_id`, `convention_id`, `version_id`, `sender_id`, `role_id`, `created_on`)",
+    " VALUES ",
+        "(?, ?, (SELECT convention_id from sq_events where sq_events.event_id = ?), ?, ?, ?, ?)",
+    ";"
 ].join('');
 
 var connection;
@@ -19,50 +19,50 @@ var telegram;
 
 // NotificationService
 var addNotification = function(typeId, eventId, versionId, senderId, roleId) {
-	var now = new Date();
-	var params = [
-    typeId,
-    eventId,
-    eventId,
-    versionId,
-    senderId,
-    roleId,
-    util.getTimeJStoSQL(now)
-  ];
-	connection.query(INSERT_NOTIFICATION_QUERY, params, function(err,state){
-    if(err) {
-       console.log(err);
+    var now = new Date();
+    var params = [
+        typeId,
+        eventId,
+        eventId,
+        versionId,
+        senderId,
+        roleId,
+        util.getTimeJStoSQL(now)
+    ];
+    connection.query(INSERT_NOTIFICATION_QUERY, params, function(err,state){
+        if(err) {
+            console.log(err);
+        }
+    });
+    if (isTelegramActive) {
+        telegram.notify(typeId, eventId, versionId, senderId, roleId);
     }
-  });
-	if (isTelegramActive) {
-		telegram.notify(typeId, eventId, versionId, senderId, roleId);
-	}
-}
+};
 
 module.exports = function(telegramIntegration, databaseConnection) {
-  connection = databaseConnection || {};
-  telegram = telegramIntegration;
+    connection = databaseConnection || {};
+    telegram = telegramIntegration;
 
-  if (connection.query &&
-      typeof connection.query !== 'function'
-  ) {
-    connection.query = function () {};
-    console.warn('WARNING: No valid "database connection" available for "notificationService".');
-  }
+    if (connection.query &&
+        typeof connection.query !== 'function'
+    ) {
+        connection.query = function () {};
+        console.warn('WARNING: No valid "database connection" available for "notificationService".');
+    }
 
-  if (typeof telegram !== 'undefined') {
-	  connection.query(CHECK_TELEGRAM_ACTIVE_QUERY, function (err, result) {
-      if (result &&
-          result.length &&
-          result[0].configuration_value
-      ) {
-        isTelegramActive = true;
-      }
-	  });
-  }
+    if (typeof telegram !== 'undefined') {
+        connection.query(CHECK_TELEGRAM_ACTIVE_QUERY, function (err, result) {
+            if (result &&
+                result.length &&
+                result[0].configuration_value
+            ) {
+                isTelegramActive = true;
+            }
+        });
+    }
 
-  return {
-    active: isTelegramActive,
-    notify: addNotification
-  };
+    return {
+        active: isTelegramActive,
+        notify: addNotification
+    };
 };
--- app/lib/utilities.js
+++ app/lib/utilities.js
@@ -1,38 +1,38 @@
 module.exports = {
-// Handle Times
-  getTimeSQLtoJS: function (timeString) {
-    // Split timestamp into [ Y, M, D, h, m, s ]
-    var t = timeString.split(/[- :]/);
+    // Handle Times
+    getTimeSQLtoJS: function (timeString) {
+        // Split timestamp into [ Y, M, D, h, m, s ]
+        var t = timeString.split(/[- :]/);
 
-    // Apply each element to the Date function
-    var d = new Date(Date.UTC(t[0], t[1]-1, t[2], t[3], t[4], t[5]));
+        // Apply each element to the Date function
+        var d = new Date(Date.UTC(t[0], t[1]-1, t[2], t[3], t[4], t[5]));
 
-    return d;
-  },
+        return d;
+    },
 
-  getTimeJStoSQL: function (timeString) {
-    // Your default date object
-    var starttime = new Date();
-    // Get the iso time (GMT 0 == UTC 0)
-    var isotime = new Date((new Date(timeString)).toISOString() );
-    // getTime() is the unix time value, in milliseconds.
-    // getTimezoneOffset() is UTC time and local time in minutes.
-    // 60000 = 60*1000 converts getTimezoneOffset() from minutes to milliseconds.
-    var fixedtime = new Date(isotime.getTime()-(starttime.getTimezoneOffset()*60000));
-    // toISOString() is always 24 characters long: YYYY-MM-DDTHH:mm:ss.sssZ.
-    // .slice(0, 19) removes the last 5 chars, ".sssZ",which is (UTC offset).
-    // .replace('T', ' ') removes the pad between the date and time.
-    var formatedMysqlString = fixedtime.toISOString().slice(0, 19).replace('T', ' ');
-    return formatedMysqlString;
-  },
+    getTimeJStoSQL: function (timeString) {
+        // Your default date object
+        var starttime = new Date();
+        // Get the iso time (GMT 0 == UTC 0)
+        var isotime = new Date((new Date(timeString)).toISOString() );
+        // getTime() is the unix time value, in milliseconds.
+        // getTimezoneOffset() is UTC time and local time in minutes.
+        // 60000 = 60*1000 converts getTimezoneOffset() from minutes to milliseconds.
+        var fixedtime = new Date(isotime.getTime()-(starttime.getTimezoneOffset()*60000));
+        // toISOString() is always 24 characters long: YYYY-MM-DDTHH:mm:ss.sssZ.
+        // .slice(0, 19) removes the last 5 chars, ".sssZ",which is (UTC offset).
+        // .replace('T', ' ') removes the pad between the date and time.
+        var formatedMysqlString = fixedtime.toISOString().slice(0, 19).replace('T', ' ');
+        return formatedMysqlString;
+    },
 
-  // generate uniqueCode
-  generateCode: function (codeLength) {
-    var allowed = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    var code = "";
-    for (var i = 0; i < codeLength; i++) {
-      code += allowed[parseInt(Math.random() * allowed.length)];
+    // generate uniqueCode
+    generateCode: function (codeLength) {
+        var allowed = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        var code = "";
+        for (var i = 0; i < codeLength; i++) {
+            code += allowed[parseInt(Math.random() * allowed.length)];
+        }
+        return code;
     }
-    return code;
-  }
 }
