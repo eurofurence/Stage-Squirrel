f3df98c730eb38e49ecb0f2300d195e8b5f628e1
Kandril <kandril.kanusky@gmail.com>
2017-07-14 23:23:47 +0200

    #15 Move /manage route to separate file

--- app/routes.js
+++ app/routes.js
@@ -27,39 +27,9 @@ module.exports = function(app, passport) {
     require('./routes/create')(app, passport, connection, notifier);
     require('./routes/home')(app, passport, connection);
     require('./routes/login')(app, passport);
+    require('./routes/manage')(app, passport, connection);
     require('./routes/profile')(app, passport, connection, notifier);
     require('./routes/signup')(app, passport, connection);
-
-    // =====================================
-    // MANAGE EVENTS =======================
-    // =====================================
-    // show the admin config
-    app.get('/manage', isLoggedIn, function(req, res) {
-    if (req.user.isManager) {
-        connection.query('SELECT * from sq_role where role_is_active = 1', function (err, rolerows) {
-          connection.query('SELECT * from sq_user where user_active = 1 order by user_name', function (err, userrows) {
-            connection.query('SELECT * FROM sq_events left join sq_event_details on sq_events.event_id = sq_event_details.event_id AND sq_events.event_max_version = sq_event_details.event_version', function (err, eventrows) {
-              connection.query('SELECT * FROM sq_riders', function (err, riderrows) {
-                connection.query('SELECT *, max(version) FROM stagesquirrel.sq_map_riders_to_roles group by event_id, role_id', function (err, riderrolesrows) {
-                res.render('manage.ejs', {
-                  riderroles: riderrolesrows,
-                  riders: riderrows,
-                nav: 'manage',
-                        user  : req.user,
-                  users : userrows,
-                  events: eventrows,
-                    roles : rolerows
-                  });
-                });
-              });
-            });
-          });
-        });
-
-    } else {
-      res.redirect('/home');
-    }
-    });
 
 
     // =====================================
@@ -349,19 +319,6 @@ module.exports = function(app, passport) {
       }
 
       res.redirect('/stages');
-    } else {
-      res.redirect('/home');
-    }
-    });
-
-    // =====================================
-    // MANAGE EVENTS =======================
-    // =====================================
-    // show the admin config
-    app.post('/manage', isLoggedIn, function(req, res) {
-    if (req.user.isManager) {
-
-
     } else {
       res.redirect('/home');
     }
