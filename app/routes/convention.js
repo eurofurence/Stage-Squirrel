f60daa95f3d03471de8297290dc655b7fe216f6b
Kandril <kandril.kanusky@gmail.com>
2017-12-24 08:27:43 +0100

    #21 Use `insertId` returned by INSERT-statement for mapping

--- app/routes/convention.js
+++ app/routes/convention.js
@@ -4,16 +4,16 @@ var isLoggedIn = require('../middleware/isLoggedIn');
 // =====================================
 // MANAGE CONVENTIONS ==================
 // =====================================
-module.exports = function (app, passport, connection) {
+module.exports = function(app, passport, connection) {
     // show the admin config
-    app.get('/convention', isLoggedIn, function (req, res) {
+    app.get('/convention', isLoggedIn, function(req, res) {
         if (req.user.isManager) {
-            connection.query('SELECT * from sq_conventions order by date_from ASC', function (err, conventionrows) {
-                connection.query('SELECT * from sq_user where user_active = 1 order by user_name', function (err, userrows) {
-                    connection.query('SELECT * from sq_events order by event_created DESC', function (err, eventrows) {
-                        connection.query('SELECT * from sq_form_templates order by template_created DESC', function (err, templaterows) {
-                            connection.query('SELECT * FROM sq_map_convention_to_stage', function (err, mapstagerows) {
-                                connection.query('SELECT * FROM sq_stage', function (err, stagerows) {
+            connection.query('SELECT * from sq_conventions order by date_from ASC', function(err, conventionrows) {
+                connection.query('SELECT * from sq_user where user_active = 1 order by user_name', function(err, userrows) {
+                    connection.query('SELECT * from sq_events order by event_created DESC', function(err, eventrows) {
+                        connection.query('SELECT * from sq_form_templates order by template_created DESC', function(err, templaterows) {
+                            connection.query('SELECT * FROM sq_map_convention_to_stage', function(err, mapstagerows) {
+                                connection.query('SELECT * FROM sq_stage', function(err, stagerows) {
                                     res.render('convention.ejs', {
                                         nav: 'convention',
                                         user: req.user,
@@ -36,18 +36,18 @@ module.exports = function (app, passport, connection) {
     });
 
 
-    app.post('/convention', isLoggedIn, function (req, res) {
+    app.post('/convention', isLoggedIn, function(req, res) {
         if (req.user.isManager) {
             console.log(req.body);
             if (req.body.actionType == "create") {
                 var params = [req.body.convention_template, req.body.convention_name, req.body.convention_desc, new Date(req.body.start), new Date(req.body.end)];
                 if (req.body.convention_id == 0) {
-                    connection.query("INSERT INTO sq_conventions (template_id, convention_name, convention_description, date_from, date_to) VALUES (?, ?, ?, ?, ?)", params, function (err, state) {
+                    connection.query("INSERT INTO sq_conventions (template_id, convention_name, convention_description, date_from, date_to) VALUES (?, ?, ?, ?, ?)", params, function(err, state) {
                         if (err) {
                             console.log(err);
                         }
                         for (stage_id of req.body.convention_stage) {
-                            connection.query("INSERT INTO sq_map_convention_to_stage (convention_id, stage_id) VALUES ('" + req.body.convention_id + "', '" + stage_id + "')", function (err, state) {
+                            connection.query("INSERT INTO sq_map_convention_to_stage (convention_id, stage_id) VALUES ('" + state.insertId + "', '" + stage_id + "')", function(err, state) {
                                 if (err) {
                                     console.log(err);
                                 }
@@ -55,16 +55,16 @@ module.exports = function (app, passport, connection) {
                         }
                     });
                 } else {
-                    connection.query("UPDATE sq_conventions SET template_id = ?, convention_name = ?, convention_description = ?, date_from = ?, date_to = ? WHERE `convention_id` = '" + req.body.convention_id + "'", params, function (err, state) {
+                    connection.query("UPDATE sq_conventions SET template_id = ?, convention_name = ?, convention_description = ?, date_from = ?, date_to = ? WHERE `convention_id` = '" + req.body.convention_id + "'", params, function(err, state) {
                         if (err) {
                             console.log(err);
                         }
-                        connection.query("DELETE FROM sq_map_convention_to_stage WHERE `convention_id` = '" + req.body.convention_id + "'", function (err, state) {
+                        connection.query("DELETE FROM sq_map_convention_to_stage WHERE `convention_id` = '" + req.body.convention_id + "'", function(err, state) {
                             if (err) {
                                 console.log(err);
                             }
                             for (stage_id of req.body.convention_stage) {
-                                connection.query("INSERT INTO sq_map_convention_to_stage (convention_id, stage_id) VALUES ('" + req.body.convention_id + "', '" + stage_id + "')", function (err, state) {
+                                connection.query("INSERT INTO sq_map_convention_to_stage (convention_id, stage_id) VALUES ('" + req.body.convention_id + "', '" + stage_id + "')", function(err, state) {
                                     if (err) {
                                         console.log(err);
                                     }
@@ -74,13 +74,13 @@ module.exports = function (app, passport, connection) {
                     });
                 }
             } else if (req.body.actionType == "delete") {
-                connection.query("SELECT count(*) AS qty from sq_events WHERE convention_id = '" + req.body.convention_id + "'", function (err, events) {
+                connection.query("SELECT count(*) AS qty from sq_events WHERE convention_id = '" + req.body.convention_id + "'", function(err, events) {
                     if (events[0].qty == 0) {
-                        connection.query("DELETE FROM sq_conventions WHERE `convention_id` = '" + req.body.convention_id + "'", function (err, state) {
+                        connection.query("DELETE FROM sq_conventions WHERE `convention_id` = '" + req.body.convention_id + "'", function(err, state) {
                             if (err) {
                                 console.log(err);
                             }
-                            connection.query("DELETE FROM sq_map_convention_to_stage WHERE `convention_id` = '" + req.body.convention_id + "'", function (err, state) {
+                            connection.query("DELETE FROM sq_map_convention_to_stage WHERE `convention_id` = '" + req.body.convention_id + "'", function(err, state) {
                                 if (err) {
                                     console.log(err);
                                 }
