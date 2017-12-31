28e815908d68d40a0831a6e4e4c85379c2585c63
Kandril <kandril.kanusky@gmail.com>
2017-12-31 18:40:02 +0100

    #16 Implement transaction handling and multi-queries for DB aware ViewModels

--- app/lib/ViewModelBase/DatabaseAware.js
+++ app/lib/ViewModelBase/DatabaseAware.js
@@ -1,5 +1,6 @@
-var util = require('util');
 var LoggerAwareBase = require('./LoggerAware');
+var util = require('util');
+var waterfall = require('async/waterfall');
 
 // Construction
 var DatabaseAware = function(connection, logger) {
@@ -8,33 +9,110 @@ var DatabaseAware = function(connection, logger) {
 };
 util.inherits(DatabaseAware, LoggerAwareBase);
 
+// Helper
+DatabaseAware.prepareQueryArguments_ = function(values, successCallback, failureCallback) {
+    var queryArgs = { onFailure: function() {}, onSuccess: function() {} };
+
+    if (util.isFunction(values)) {
+        queryArgs.onSuccess = values;
+        if (util.isFunction(successCallback)) {
+            queryArgs.onFailure = successCallback;
+        }
+        return queryArgs;
+    }
+
+    if (values !== undefined) {
+        queryArgs.values = values;
+    }
+    if (util.isFunction(successCallback)) {
+        queryArgs.onSuccess = successCallback;
+    }
+    if (util.isFunction(failureCallback)) {
+        queryArgs.onFailure = failureCallback;
+    }
+    return queryArgs;
+};
+
+DatabaseAware.prepareOnFailureCall_ = function(logger, callback) {
+    var onFailure = (util.isFunction(callback) ? callback : function() {});
+    return function(error) {
+        logger.log(error);
+        onFailure(error);
+    };
+};
+
+DatabaseAware.prepareWaterfallQuery_ = function(connection, query, params) {
+    return function(resultLists, fieldLists, callback) {
+        connection.query(query, params, function(error, results, fields) {
+            if (error) {
+                return callback(error, resultLists, fieldLists);
+            }
+            resultLists.push(results);
+            fieldLists.push(fields);
+            callback(null, resultLists, fieldLists);
+        });
+    };
+};
+
 // Methods
 DatabaseAware.prototype.query = function(query, values, successCallback, failureCallback) {
-    var logger = this.logger;
-    var failureCb, params, successCb;
+    var args = DatabaseAware.prepareQueryArguments_(values, successCallback, failureCallback);
+    var onFailure = DatabaseAware.prepareOnFailureCall_(this.logger, args.onFailure);
 
-    if (typeof failureCallback === 'function') {
-        failureCb = failureCallback;
-    }
-    if (typeof values === 'function') {
-        successCb = values;
-    }
-    if (typeof successCallback === 'function' && values !== undefined) {
-        successCb = successCallback;
-        params = values;
-    }
-
-    this.connection.query(query, params, function(error, results, fields) {
+    this.connection.query(query, args.values, function(error, results, fields) {
         if (error) {
-            logger.log(error);
-            if (failureCb !== undefined) {
-                failureCb(error);
-            }
-        } else {
-            if (successCb !== undefined) {
-                successCb(results, fields);
-            }
+            return onFailure(error);
         }
+        args.onSuccess(results, fields);
+    });
+};
+
+DatabaseAware.prototype.multiQuery = function(queries, values, successCallback, failureCallback) {
+    var args = DatabaseAware.prepareQueryArguments_(values, successCallback, failureCallback);
+    var onFailure = DatabaseAware.prepareOnFailureCall_(this.logger, args.onFailure);
+
+    var queryCalls = [function(callback) {
+        callback(null, [], []);
+    }];
+    args.values = (util.isArray(args.values) ? args.values : []);
+    for (var i = 0; i < queries.length; i++) {
+        queryCalls.push(DatabaseAware.prepareWaterfallQuery_(this.connection, queries[i], args.values[i]));
+    }
+    waterfall(queryCalls, function(error, resultLists, fieldLists) {
+        if (error) {
+            return onFailure(error);
+        }
+        args.onSuccess(resultLists, fieldLists);
+    });
+};
+
+DatabaseAware.prototype.beginTransaction = function(successCallback, failureCallback) {
+    var connection = this.connection;
+    var onFailure = (util.isFunction(failureCallback) ? failureCallback : function() {});
+    var onSuccess = (util.isFunction(successCallback) ? successCallback : function() {});
+    var onError = DatabaseAware.prepareOnFailureCall_(this.logger, onFailure);
+
+    var rollback = function(error) {
+        connection.rollback(function() {
+            onFailure(error);
+        });
+    };
+    var commit = function(onCommit) {
+        connection.commit(function(error) {
+            if (error) {
+                return onError(error);
+            }
+            if (typeof onCommit === 'function') {
+                onCommit();
+            }
+        });
+    };
+
+    this.connection.beginTransaction(function(error) {
+        if (error) {
+            return onError(error);
+        }
+        onSuccess(commit, rollback);
     });
 };
 
